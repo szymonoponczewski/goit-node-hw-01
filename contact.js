bbcdcdbed208773d7a9429b9ed64ae37a835acc5
Szymon Oponczewski <szymon_o@mac.com>
2023-08-09 21:11:17 +0200

    contact.js update

--- contact.js
+++ contact.js
@@ -1,22 +1,42 @@
 const fs = require("fs");
 const path = require("path");
-
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 function listContacts() {
-  // Your code to list contacts
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading contacts:", err);
+
+      return;
+    }
+    const contacts = JSON.parse(data);
+    console.table(contacts);
+  });
 }
 
 function getContactById(contactId) {
-  // Your code to get contact by ID
+  fs.readFile(contactsPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading contacts:', err);
+      return;
+    }
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((c) => c.id === contactId);
+    if (contact) {
+      console.log('Contact found:', contact);
+    } else {
+      console.log('Contact not found.');
+    }
+  });
 }
 
-function removeContact(contactId) {
-  // Your code to remove contact
-}
+  function removeContact(contactId) {
+    // Your code to remove contact
+  }
 
-function addContact(name, email, phone) {
-  // Your code to add contact
-}
+  function addContact(name, email, phone) {
+    // Your code to add contact
+  }
 
-module.exports = { listContacts, getContactById, removeContact, addContact };
+  module.exports = { listContacts, getContactById, removeContact, addContact };
+
